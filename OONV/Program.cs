030e6a610b5ccc054218e705f5a4938efdc4683f
elmalving <sjjsjdjd.sksksj@gmail.com>
2024-02-04 23:40:20 +0100

    Added Singleton example

--- OONV/Program.cs
+++ OONV/Program.cs
@@ -9,6 +9,10 @@
             heroBuilder.SetArmor();
             Hero hero = heroBuilder.TrainHero();
             Console.WriteLine($"{hero.Name}, {hero.Age}\nClass: {hero.Class}\nWeapon: {hero.Weapon.Name}\nArmor: {hero.Armor.Name}");
+
+            Hero hero1 = Hero.Create("", 22, Hero.Classes.KNIGHT);
+            Hero hero2 = Hero.Create("", 23, Hero.Classes.KNIGHT);
+            Console.WriteLine($"{hero1.Age}, {hero2.Age}");
         }
     }
 }
